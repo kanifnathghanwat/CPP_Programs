18b141dfdb72eda72f865f71f0df710c4ada24a3
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-08-28 09:10:19 -0900

    Add files via upload

---
+++ 2.switch_case.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, result;
+	char ch;
+	cout<<"Enter two numbers: ";
+	cin>>a>>b;
+	cout<<"Choice Operation(+, -, *, /, %): ";
+	cin>>ch;
+	switch(ch)
+	{
+		case('+'):cout<<a+b;
+		break;
+		case('-'):cout<<a-b;
+		break;
+		case('*'):cout<<a*b;
+		break;
+		case('/'):cout<<a/b;
+		break;
+		case('%'):cout<<a%b;
+		break;
+		default:cout<<"Invalid character...!";
+	}
+    return 0;
+}
