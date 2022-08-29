2f80ba6947ddf10f329d65f0a8324d501af879d0
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-08-29 08:19:27 -0900

    Add files via upload

---
+++ 3.reverse_number.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int no;
+	cout<<"Enter a number: ";
+	cin>>no;          //345
+	int rev=0, r;
+	while(no>0)
+	{
+		r=no%10;      //5, 4, 3
+		rev=rev*10+r; //5, 54, 3
+		no=no/10;     //34, 3, 0
+	}
+	cout<<"Reverse number: "<<rev;
+}
