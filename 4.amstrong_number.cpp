978cd36e6b20243282020bb733dbace413fda256
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-01 09:14:12 -0900

    Add files via upload

---
+++ 4.amstrong_number.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int no, no1, sum=0,rem, temp, len=0;
+	cout<<"Enter a number: ";
+	cin>>no;
+	no1=temp=no;
+	while((no)>0)
+	{
+		len=len+1;
+		no=no/10;
+	}
+	while(no1>0)
+	{
+		rem=no1%10;
+		sum=sum+(pow(rem, len)) ;
+		no1=no1/10;
+	}
+	if(temp==sum)
+	{
+		cout<<temp<<" is Armstrong number";
+	}
+    else
+	{
+		cout<<temp<<" is not Armstrong number";
+	}
+}
