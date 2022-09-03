09d602f3932211d7a267bd27fd326dcac5e1cb9f
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-03 09:28:24 -0900

    Add files via upload

---
+++ 5.digit_sum.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int no, d, sum=0;
+	cout<<"Enter a number: ";
+	cin>>no;
+	while(no>0)
+	{
+		d=no%10;
+		sum=sum+d;
+		no=no/10;
+	}
+	cout<<sum;
+}
