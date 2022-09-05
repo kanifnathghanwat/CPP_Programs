c4b1ad0b0e27929938562ddfff381a2951c63d1c
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-05 04:24:31 -0900

    Add files via upload

---
+++ 6.class_arithmetic_operation.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+using namespace std;
+class base
+{
+    public:
+    int a,b;
+    base()
+    {
+        a=30;
+        b=20;
+    }
+    void add()
+    {
+        cout<<"Adition is= "<<a+b<<endl;
+    }
+    void mul()
+    {
+        cout<<"Multiplication is= "<<a*b<<endl;
+    }
+};
+class child:public base
+{
+    public:
+    void div()
+    {
+        cout<<"Division is= "<<a/b<<endl;
+    }
+    void mod()
+    {
+        cout<<"Modulos is= "<<a%b<<endl;
+    }
+};
+int main()
+{
+    base s1;
+    child s2;
+    s2.add();
+    s2.mul();
+    s2.div();
+    s2.mod();
+}
