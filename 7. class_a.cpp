a182ef327ac02b55572e3c93786f3f7f573b4f4c
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-10 04:34:37 -0900

    Add files via upload

---
+++ 7. class_a.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+class a
+{
+public:
+    a()
+    {
+        cout<<"bca  \n";
+    }
+};
+class b : public a
+{
+public:
+     b()
+    {
+        cout<<"bcs   \n" ;
+    }
+    void print()
+    {
+        printf("print");
+    }
+};
+int main()
+{
+    b ob;
+    ob.print();
+}
