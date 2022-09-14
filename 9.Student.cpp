6744f6305623c488405ea6cbc589e1ae30b09293
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-14 04:44:24 -0900

    Add files via upload

---
+++ 9.Student.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>  
+using namespace std;  
+class Student {  
+   public:  
+      int id=10;//data member (also instance variable)      
+      string name="kanifnath";//data member(also instance variable)      
+};  
+int main() {  
+    Student s1; //creating an object of Student   
+   // s1.id = 201;    
+   // s1.name = "XYZ";   
+    cout<<s1.id<<endl;  
+    cout<<s1.name<<endl;  
+    return 0;  
+}  
