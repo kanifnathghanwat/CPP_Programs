4cfe66ef4900a13540d0315d5defe34c9f7f4618
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-09-27 08:23:40 -0900

    Add files via upload

---
+++ 14.parameterised_constructor.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+class Car 
+{        // The class
+    public:          // Access specifier
+    string brand;  // Attribute
+    string model;  // Attribute
+    int year;      // Attribute
+    Car(string x, string y, int z) 
+    {  // Constructor with parameters
+       brand = x;
+       model = y;
+       year = z;
+    }
+};
+
+int main() 
+{
+  // Create Car objects and call the constructor with different values
+    Car Obj1("BMW", "X5", 1999);
+    Car Obj2("Ford", "Mustang", 1969);
+
+   // Print values
+    cout<<Obj1.brand<<" "<<Obj1.model<<" "<<Obj1.year<<"\n";
+    cout<<Obj2.brand<<" "<<Obj2.model<<" "<<Obj2.year<<"\n";
+    return 0;
+}
