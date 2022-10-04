bb05cba658034b8de14b7068276156c7fe3e3f98
Ghanwat Kanifnath Adinath <110013661+kanifnathghanwat@users.noreply.github.com>
2022-10-04 07:39:16 -0900

    Add files via upload

---
+++ 18.multi_level_inheritance.cpp
@@ -0,0 +1,38 @@
+﻿/* Title: Multi-level inheritance
+   Developer: Kanifnath Ghanwat
+   Date: 17/09/2022
+*/
+#include <iostream>  
+using namespace std;  
+class A
+{  
+   public:  
+   void eat() 
+   {   
+       cout<<"Eating..."<<endl;   
+   }    
+};  
+class B: public A   
+{    
+    public:  
+    void bark()
+    {  
+        cout<<"Barking..."<<endl;   
+    }    
+};   
+class C: public B  
+{    
+    public:  
+    void weep()
+    {  
+         cout<<"Weeping...";   
+    }    
+};   
+int main(void) 
+{  
+    C obj;  
+    obj.eat();  
+    obj.bark();  
+    obj.weep();  
+    return 0;  
+}  
